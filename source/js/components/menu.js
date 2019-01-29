292169351a270089251ba5f285c779c6ae907df4
Felipe Ferreira <felipe.ferreira.ads@gmail.com>
2019-01-29 11:54:38 -0200

    add css home

---
+++ source/js/components/menu.js
@@ -0,0 +1,10 @@
+$('.open-menu').on('click', function(e){
+    e.preventDefault()
+    $('.menu-mobile').css('right', 0)
+    $('.overlay-menu').fadeIn(300)
+})
+
+$('.overlay-menu, .close-menu').on('click', function(e){
+    $('.menu-mobile').css('right', -300)
+    $('.overlay-menu').fadeOut(300)
+})
