c496cd842303ce62c5046735bd45ec4f57a0052e
Felipe Ferreira <felipe.ferreira.ads@gmail.com>
2019-01-30 11:53:00 -0200

    add menu

--- source/js/components/menu.js
+++ source/js/components/menu.js
@@ -1,7 +1,34 @@
 $('.open-menu').on('click', function(e){
-    e.preventDefault()
-    $('.menu-mobile').css('right', 0)
-    $('.overlay-menu').fadeIn(300)
+    if(!$('.main-menu').hasClass('open')) {
+        $('.main-menu').css({
+            left: 0
+        }).addClass('open')
+
+        $('header').css({
+            left: 300
+        })
+
+        $('header span').html('Fechar')
+
+        $('.menu-switch').css({
+            left: 300
+        })
+    } else {
+        console.log('oi')
+        $('.main-menu').css({
+            left: -300
+        }).removeClass('open')
+
+        $('header').css({
+            left: 0
+        })
+
+        $('header span').html('Menu')
+
+        $('.menu-switch').css({
+            left: 0
+        })
+    }
 })
 
 $('.overlay-menu, .close-menu').on('click', function(e){
--- source/js/index.js
+++ source/js/index.js
@@ -3,3 +3,4 @@
  */
 
 import './components/scroll'
+import './components/menu'
